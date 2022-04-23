6ae08c65ed024c4068db80100478bfed98beb471
Xintong <xintongyang1994@foxmail.com>
2022-04-23 09:35:19 +0100

    new exps

--- run_lv_cr-0.01.sh
+++ run_lv_cr-0.01.sh
@@ -1,6 +1,12 @@
-python envtest/python/train_vision_ppo.py --crcoeff -0.01 --lvcoeff 0.0 --posxcoeff -0.0001 --avcoeff 0.0
 python envtest/python/train_vision_ppo.py --crcoeff -0.01 --lvcoeff 0.0 --posxcoeff -0.001 --avcoeff 0.0
 python envtest/python/train_vision_ppo.py --crcoeff -0.01 --lvcoeff 0.0 --posxcoeff -0.005 --avcoeff 0.0
-python envtest/python/train_vision_ppo.py --crcoeff -0.05 --lvcoeff 0.0 --posxcoeff -0.0001 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.01 --lvcoeff 0.0 --posxcoeff -0.01 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.01 --lvcoeff 0.0 --posxcoeff -0.05 --avcoeff 0.0
 python envtest/python/train_vision_ppo.py --crcoeff -0.05 --lvcoeff 0.0 --posxcoeff -0.001 --avcoeff 0.0
 python envtest/python/train_vision_ppo.py --crcoeff -0.05 --lvcoeff 0.0 --posxcoeff -0.005 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.05 --lvcoeff 0.0 --posxcoeff -0.01 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.05 --lvcoeff 0.0 --posxcoeff -0.05 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.1 --lvcoeff 0.0 --posxcoeff -0.001 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.1 --lvcoeff 0.0 --posxcoeff -0.005 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.1 --lvcoeff 0.0 --posxcoeff -0.01 --avcoeff 0.0
+python envtest/python/train_vision_ppo.py --crcoeff -0.1 --lvcoeff 0.0 --posxcoeff -0.05 --avcoeff 0.0
