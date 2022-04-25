18526c66f92ff9bfa0d85089336f8f5c90b7da6d
Xintong <xintongyang1994@foxmail.com>
2022-04-25 22:57:24 +0100

    wrong distance r coeff

--- run_crt_0.01.sh
+++ run_crt_0.01.sh
@@ -1,2 +1,2 @@
-python envtest/python/train_vision_ppo.py --collision_terminal_coeff 0.01 --posxcoeff -0.3
-python envtest/python/train_vision_ppo.py --collision_terminal_coeff 0.01 --posxcoeff -0.35
+python envtest/python/train_vision_ppo.py --collision_terminal_coeff 0.01 --posxcoeff 0.3
+python envtest/python/train_vision_ppo.py --collision_terminal_coeff 0.01 --posxcoeff 0.35
