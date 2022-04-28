1f57a05a853d54d36947824fafe5e49702acec5c
xintong <xintongyang1994@foxmail.com>
2022-04-28 08:18:51 +0100

    results

---
+++ run_crt_0.01_custom.sh
@@ -0,0 +1,6 @@
+python envtest/python/train_vision_ppo.py --level custom --collision_terminal_coeff 0.01 --crcoeff -0.01 --posxcoeff 0.4
+python envtest/python/train_vision_ppo.py --level custom --collision_terminal_coeff 0.01 --crcoeff -0.01 --posxcoeff 0.45
+python envtest/python/train_vision_ppo.py --level custom --collision_terminal_coeff 0.01 --crcoeff -0.005 --posxcoeff 0.4
+python envtest/python/train_vision_ppo.py --level custom --collision_terminal_coeff 0.01 --crcoeff -0.005 --posxcoeff 0.45
+python envtest/python/train_vision_ppo.py --level custom --collision_terminal_coeff 0.01 --crcoeff -0.001 --posxcoeff 0.4
+python envtest/python/train_vision_ppo.py --level custom --collision_terminal_coeff 0.01 --crcoeff -0.001 --posxcoeff 0.45
